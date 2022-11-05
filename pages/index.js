090f440834d4eb2ac832f99f719b06fb58c7ebc5
SathyakumarDev <zenmedi0023@gmail.com>
2022-11-06 00:00:37 +0530

    product page created

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,9 @@ import {Product, FooterBanner, HeroBanner} from '../components'
 const Home = ({ products, bannerData}) => (
     <>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
-      {console.log(bannerData)}
+
       <div className="products-heading">
-        <h2>Beset Selling Products</h2>
+        <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
