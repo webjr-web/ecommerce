10c8ab733847c9ec553603a71863e01c76e784d8
SathyakumarDev <zenmedi0023@gmail.com>
2022-11-05 19:57:18 +0530

    backend linked

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,25 +1,29 @@
 import Link from "next/link";
 import React from "react";
 
-const HeroBanner = () => {
+import {urlFor} from '../lib/client'
+
+const HeroBanner = ({heroBanner}) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img className="hero-banner-image" src="" alt="headphones" />
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+
+        <img className="hero-banner-image" src={urlFor(heroBanner.image)} alt="headphones" />
 
         <div>
-          <Link href="/product/ID">
-            <button type="button">Button Text</button>
+          <Link href={`/product/ID/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>Description</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
-    </div>
+    </div> 
   );
 };
 
